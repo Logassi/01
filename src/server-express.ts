e463cc0df505d17afc9490eb8091fa7f202ceba0
Logassi <lomonagagaspas@gmail.com>
2024-10-21 20:17:35 +0700

    Feat : Creating connection to MOCK-API, and instaling axios

--- src/server-express.ts
+++ src/server-express.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, Application } from "express";
-import { PORT as port } from "./config";
+import { PORT as port, MOCK_API_URL } from "./config";
+import axios from "axios";
 
 const PORT = Number(port) || 8000;
 
@@ -13,16 +14,24 @@ const app: Application = express();
 // });
 
 // untuk mendapatkan semua data users
-app.get("/api/users", (req: Request, res: Response) => {
+app.get("/api/users", async (req: Request, res: Response) => {
+  // const { id } = req.params;
+  const { data } = await axios.get(`${MOCK_API_URL}/users`);
+
   res.status(200).send({
     message: "Ini List Users",
+    data,
   });
 });
 
 // untuk mendapatkan satu data users
-app.get("/api/users/:id", (req: Request, res: Response) => {
+app.get("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { data } = await axios.get(`${MOCK_API_URL}/users/${id}`);
+
   res.status(200).send({
     message: "Ini Data 1 User",
+    data,
   });
 });
 
@@ -34,11 +43,8 @@ app.post("/api/users", (req: Request, res: Response) => {
 });
 
 app.patch("/api/users/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-
   res.status(200).send({
     message: "Ini Edit Data Users",
-    data: id,
   });
 });
 
